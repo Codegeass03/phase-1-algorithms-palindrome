c5c5489827e5400976e12f2c330c9617a1f417e3
Vivian Cheruiyot <viviancheruiyot32@gmail.com>
2025-08-12 17:32:55 +0300

    palindrome solution

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+
+  if(word === reversedWord){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  1. Split the word into an array of characters
+  2. Reverse the array of characters
+  3. Join the reversed array back into a string
+  4. Compare the original word with the reversed word
+  5. Return true if they are the same, otherwise return false
 */
 
 /*
   Add written explanation of your solution here
+  1. The function takes a string input `word`.
+  2. I declared a variable `reversedWord` that stores the reversed version of the input word.
+  3. It uses the `split("")` method to convert the string into an array of characters.
+  4. It then reverses the array using `reverse()`.
+  5. Finally, it joins the reversed array back into a string using `join("")`.
+  6. Used the if-statement to check if the original word is equal to the reversed word.
+  7. If they are equal, it returns true, indicating that the word is a palindrome.
+  8. If they are not equal, it returns false, indicating that the word is not a palindrome. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +40,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("rotator"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("rear"));
 }
 
 module.exports = isPalindrome;
--- test/indexTest.js
+++ test/indexTest.js
@@ -18,4 +18,7 @@ describe("isPalindrome", () => {
   it("returns false for 'ab'", () => {
     expect(isPalindrome("ab")).to.be.false;
   });
+  it("returns false for 'MoM'", () => {
+    expect(isPalindrome("mom")).to.be.true;
+  });
 });
